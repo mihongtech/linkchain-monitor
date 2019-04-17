d919db9288dd3fef323e0a3bcceb9645dbaee1fc
hum tan <yongxiang.tan@sao.so>
2019-04-17 14:55:30 +0800

    api server modify

--- browser/src/api/config.js
+++ browser/src/api/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const config = axios.create({
-  baseURL: 'http://10.150.17.95:8090/api/v1/explorer',
+  baseURL: 'http://122.112.249.47/api/v1/explorer',
   timeout: 10000
 })
 export default config
